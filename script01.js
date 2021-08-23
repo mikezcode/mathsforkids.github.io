c35c5b5794b6a6f6db5de882a8f691ec369e8167
tamiru-molla <tamiruhailu2424@gmail.com>
2021-08-24 00:27:29 +1200

    hahu added

---
+++ script01.js
@@ -0,0 +1,52 @@
+window.addEventListener("load",()=>{
+
+ let table= document.querySelector("table");
+ let body= document.querySelector(".container");
+ let body2= document.querySelector("body");
+ let div= document.createElement("div");
+ body.removeChild(table);
+ body.appendChild(div);
+ body2.style.height="100vh"
+ 
+ 
+  setTimeout(function run(){
+
+    setTimeout(()=>{
+
+
+      mike: {
+        console.log("i ma back")
+  let ran= Math.round(Math.random()*(4951-4608))+4608;
+    let index=ran;
+ //    mike:for (let index = 4608; index < 4952; index+=8) {
+     //  let tr= document.createElement("tr");
+     //  tr.innerHTML+=index
+       if(bt(index, 4680,4688)||bt(index, 4688,4696)||bt(index, 4696,4704)||bt(index, 4712,4720)||bt(index, 4744,4752)||bt(index, 4784,4792)||bt(index, 4800,4808)||bt(index,4856,4864)||bt(index, 4880,4888)||bt(index, 4888,4896) || (index-4615)%8==0){
+         if((index-4615)%8==0)console.log(`&#${index}`)
+         break mike;
+       }
+    
+      else div.innerHTML= `&#${index}`;
+    
+ 
+ 
+     function bt(num,first , second){
+     let ans= (num >= first) && (num<second);
+     return ans;
+   }
+       
+     }
+    
+    run();
+    },3000)
+
+  },3000)
+
+ 
+ 
+
+
+
+
+
+})
